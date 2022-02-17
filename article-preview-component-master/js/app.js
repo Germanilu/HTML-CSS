b48c308eeea56eb8bb12c28222bdc318f8750e20
Podran93 <Podran93>
2022-02-17 21:24:04 +0100

    Este es un repositorio

---
+++ article-preview-component-master/js/app.js
@@ -0,0 +1,11 @@
+//script di javascript che funziona per fare la funzione di mostrar l'elemento
+
+function mostrar() {
+    div = document.getElementById('flotante');
+    div.style.display = '';
+}
+
+function cerrar() {
+    div = document.getElementById('flotante');
+    div.style.display = 'none';
+}
